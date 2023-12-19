250e5fb738f95d43c1816adf0bcc44a191a612fc
YongYeon Kim <assavictory@gmail.com>
2023-12-19 15:24:59 +0900

    bmi 지수 계산 및 아이콘 셋팅

--- lib/result/result_screen.dart
+++ lib/result/result_screen.dart
@@ -6,25 +6,64 @@ class ResultScreen extends StatelessWidget {
 
   const ResultScreen({super.key, required this.height, required this.weight});
 
+  String _calcBmi(double bmi) {
+    String result = '저체중';
+    if (bmi >= 35) {
+      result = '고도 비만';
+    } else if (bmi >= 30) {
+      result = '2단계 비만';
+    } else if (bmi >= 25) {
+      result = '1단계 비만';
+    } else if (bmi >= 23) {
+      result = '과체중';
+    } else if (bmi >= 18.5) {
+      result = '정상';
+    }
+
+    return result;
+  }
+
+  Widget _buildIcon(double bmi) {
+    Icon icon = const Icon(
+      Icons.sentiment_dissatisfied,
+      color: Colors.green,
+      size: 100,
+    );
+
+    if (bmi >= 23) {
+      icon = const Icon(
+        Icons.sentiment_very_dissatisfied,
+        color: Colors.green,
+        size: 100,
+      );
+    } else if (bmi >= 18.5) {
+      icon = const Icon(
+        Icons.sentiment_satisfied,
+        color: Colors.green,
+        size: 100,
+      );
+    }
+
+    return icon;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final bmi = weight / ((height / 100.0) * (height / 100.0));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('결과'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              '정상',
-              style: TextStyle(fontSize: 36),
+              _calcBmi(bmi),
+              style: const TextStyle(fontSize: 36),
             ),
-            Icon(
-              Icons.sentiment_satisfied,
-              color: Colors.green,
-              size: 100,
-            )
+            _buildIcon(bmi)
           ],
         ),
       ),
